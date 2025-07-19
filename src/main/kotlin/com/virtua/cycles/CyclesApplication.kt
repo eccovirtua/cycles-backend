c2c937d49f1bca027863aea8bfe5a1a82bafb2a1
Martin M. <drekgx@gmail.com>
2025-07-19 14:17:19 -0400

    local .env variables addedv2

--- src/main/kotlin/com/virtua/cycles/CyclesApplication.kt
+++ src/main/kotlin/com/virtua/cycles/CyclesApplication.kt
@@ -1,5 +1,8 @@
 package com.virtua.cycles
 
+
+import io.github.cdimascio.dotenv.dotenv
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +11,9 @@ class CyclesApplication
 
 fun main(args: Array<String>) {
 	runApplication<CyclesApplication>(*args)
+	val dotenv = dotenv()
+	System.setProperty("MONGODB_URI", dotenv["MONGODB_URI"])
+
 }
+
+
--- src/main/kotlin/com/virtua/cycles/controller/ActivityController.kt
+++ src/main/kotlin/com/virtua/cycles/controller/ActivityController.kt
@@ -1,20 +1,20 @@
-package com.virtua.cycles.controller
-
-import com.virtua.cycles.model.Activity
-import com.virtua.cycles.repository.ActivityRepository
-import org.springframework.web.bind.annotation.*
-
-@RestController
-@RequestMapping("/activities")
-class ActivityController(private val repository: ActivityRepository) {
-
-    @PostMapping
-    fun saveActivity(@RequestBody activity: Activity): Activity {
-        return repository.save(activity)
-    }
-
-    @GetMapping
-    fun getAllActivities(): List<Activity> {
-        return repository.findAll()
-    }
-}
+//package com.virtua.cycles.controller
+//
+//import com.virtua.cycles.model.Activity
+//import com.virtua.cycles.repository.ActivityRepository
+//import org.springframework.web.bind.annotation.*
+//
+//@RestController
+//@RequestMapping("/activities")
+//class ActivityController(private val repository: ActivityRepository) {
+//
+//    @PostMapping
+//    fun saveActivity(@RequestBody activity: Activity): Activity {
+//        return repository.save(activity)
+//    }
+//
+//    @GetMapping
+//    fun getAllActivities(): List<Activity> {
+//        return repository.findAll()
+//    }
+//}
