2c464ee31363c29cfdf522ae7f4749e20bbce62e
Martin M. <drekgx@gmail.com>
2025-07-19 18:23:53 -0400

    Backend with Docker v1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
 rootProject.name = "cycles"
+
+// Plugin para descargar toolchains desde adoptium
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+
