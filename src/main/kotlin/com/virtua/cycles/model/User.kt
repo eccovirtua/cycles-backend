830a1a007acb53240114a345cfeac369d5c52efd
Martin M. <drekgx@gmail.com>
2025-07-18 23:11:20 -0400

    conexión exclusiva con BD atlas totalmente funcional de momento para el módulo de registro de usuarios

--- src/main/kotlin/com/virtua/cycles/model/User.kt
+++ src/main/kotlin/com/virtua/cycles/model/User.kt
@@ -1,6 +1,7 @@
+//clase user. esta clase maneja la estructura principal de un usuario de la app.
 package com.virtua.cycles.model
 
-
+import jakarta.validation.constraints.NotBlank
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
@@ -9,12 +10,17 @@ import java.time.LocalDateTime
 data class User(
     @Id val id: String? = null,
 
+    @field:NotBlank(message = "User name cannot be blank") //validaciones de campo no rellenado
     val name: String,
+
+    @field:NotBlank(message = "el correo no puede estar vacío")
     val email: String,
+
     val age: Int? = null,
+
+    @field:NotBlank(message = "la contraseña es obligatoria")
     val password: String,
 
     val cratedAt: LocalDateTime = LocalDateTime.now(),
     val updatedAt: LocalDateTime = LocalDateTime.now()
-
 )
