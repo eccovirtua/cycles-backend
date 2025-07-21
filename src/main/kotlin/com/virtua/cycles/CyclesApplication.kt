74f58ba7b0ea87b1c282c381c5351b1cf9e6a486
Martin M. <drekgx@gmail.com>
2025-07-21 12:03:50 -0400

    fixed

--- src/main/kotlin/com/virtua/cycles/CyclesApplication.kt
+++ src/main/kotlin/com/virtua/cycles/CyclesApplication.kt
@@ -2,7 +2,8 @@ package com.virtua.cycles
 
 
 import io.github.cdimascio.dotenv.dotenv
-
+//import io.jsonwebtoken.security.Keys
+//import java.util.Base64
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -13,7 +14,8 @@ fun main(args: Array<String>) {
 	runApplication<CyclesApplication>(*args)
 	val dotenv = dotenv()
 	System.setProperty("MONGODB_URI", dotenv["MONGODB_URI"])
-
+//	val key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256)
+//	println(Base64.getEncoder().encodeToString(key.encoded))
 }
 
 
